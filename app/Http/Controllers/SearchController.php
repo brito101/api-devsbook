72af48d823be35d182f13fc0bb7287db491d6fde
brito101 <rodrigobrito101@hotmail.com>
2022-07-20 22:13:29 -0300

    ADD search user method

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,5 +14,31 @@ class SearchController extends Controller
     {
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
+    }
+
+    public function search(Request $request)
+    {
+        // GET api/search (txt)
+        $array = ['error' => '', 'users' => []];
+
+        $txt = $request->input('txt');
+
+        if ($txt) {
+
+            // Busca de usuários
+            $userList = User::where('name', 'like', "%{$txt}%")->get();
+            foreach ($userList as $userItem) {
+                $array['users'][] = [
+                    'id' => $userItem['id'],
+                    'name' => $userItem['name'],
+                    'avatar' => url('storage/media/avatars/' . $userItem['avatar'])
+                ];
+            }
+        } else {
+            $array['error'] = 'Digite alguma coisa para buscar.';
+            return $array;
+        }
+
+        return $array;
     }
 }
